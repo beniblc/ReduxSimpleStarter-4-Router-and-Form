9198faeb49c41e86c5bf59bb2715719b0dca0023
Ben Crisostomo <beni.blc@gmail.com>
2017-11-01 23:05:36 -0400

    added delete and view post functionality

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { createPost } from '../actions';
 
 class PostsNew extends Component { 
     renderField(field) {
+    const { meta: {touched, error } } = field;    
+    const className = `form-group ${touched && error? 'has-danger': ''}`;
+
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                 className="form-control"
                     type="text"
                     {...field.input}
                 />
-                {field.meta.error}
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
         );    
     }
 
     onSubmit(values) {
-        console.log(values);
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -43,6 +54,7 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Canel</Link>
             </form>
         )
     }
@@ -50,9 +62,9 @@ class PostsNew extends Component {
 
 function validate(values) {
     const errors = {};
-    !values.title  ? errors.title = "Enter a title!" : 
-    !values.categories ? errors.categories = "Enter some categories!" :
-    !values.content ? errors.content = "Enter some content!" :
+    !values.title  ? errors.title = "Enter a title!" : ''
+    !values.categories ? errors.categories = "Enter some categories!" :''
+    !values.content ? errors.content = "Enter some content!" :''
     console.log("ok")
 
     return errors;
@@ -61,4 +73,6 @@ function validate(values) {
 export default reduxForm({
     validate: validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
